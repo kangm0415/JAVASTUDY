bcc3149afeb20dd7fd23b4738dab7f28117c7a11
강민 <@121.139.213.160>
2018-06-22 00:55:03 +0900

    변수, 반복문(while, for), Scanner 사용 --> 구구단

---
+++ src/CHAPTER/Gugudan.java
@@ -0,0 +1,58 @@
+package CHAPTER;
+import java.util.Scanner;
+
+public class Gugudan {
+	public static void main(String[] arge) {
+		//6단
+		
+		int X=1;
+		while(X<=10) {
+			System.out.println(6*X);
+			X+=1;
+		}
+		
+		for (int a=1; a<10; a++) {
+			System.out.println(7*a);
+		}
+		
+		//2단
+/*		System.out.println("2단");
+		System.out.println(2*1);
+		System.out.println(2*2);
+		System.out.println(2*3);
+		System.out.println(2*4);
+		System.out.println(2*5);
+		System.out.println(2*6);
+		System.out.println(2*7);
+		System.out.println(2*8);
+		System.out.println(2*9);
+		
+		//3단
+		System.out.println("3단");
+		System.out.println(3*1);
+		System.out.println(3*2);
+		System.out.println(3*3);
+		System.out.println(3*4);
+		System.out.println(3*5);
+		System.out.println(3*6);
+		System.out.println(3*7);
+		System.out.println(3*8);
+		System.out.println(3*9);
+		*/
+		//4단
+/*		System.out.println("구구단으로 출력한 단은?:  ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number*1);
+		System.out.println(number*2);
+		System.out.println(number*3);
+		System.out.println(number*4);
+		System.out.println(number*5);
+		System.out.println(number*6);
+		System.out.println(number*7);
+		System.out.println(number*8);
+		System.out.println(number*9);	*/
+		//Scanner 메소드 사용? 무튼 그거.
+		
+	}
+}
