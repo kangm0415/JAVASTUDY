54866659f9a941dd86b9ff6c75f5942ecc95ef2f
강민 <@211.252.2.193>
2018-06-23 23:49:31 +0900

    While문 기본

---
+++ src/CHAPTER5/WhileBasic.java
@@ -0,0 +1,14 @@
+package CHAPTER5;
+
+public class WhileBasic {
+
+	public static void main(String[] args) {
+		int num = 0;
+		while(num<5) {//num<5 를 만족하면 반복 실행
+			System.out.println("I like Java" + num);
+			num++;
+		}
+
+	}
+
+}
