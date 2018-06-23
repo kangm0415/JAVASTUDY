36b157576430d7b88f01f17c78493e825dbed151
강민 <@211.252.2.193>
2018-06-23 19:39:55 +0900

    주석추가

--- src/CHAPTER5/IEBasic.java
+++ src/CHAPTER5/IEBasic.java
@@ -6,11 +6,11 @@ public class IEBasic {
 		
 	        int n1 = 5;
 	        int n2 = 7; 
-	        
+	        //if
 	        if(n1 < n2) {
 	            System.out.println("n1 > n2 is true");
 	        }
-	        
+	        //if~else
 	        if(n1 == n2) {
 	            System.out.println("n1 == n2 is true");
 	        } 
