e12f98956e4e3f42244bf72505f67b9d391d7782
강민 <@211.252.2.193>
2018-06-23 23:53:59 +0900

    do~while문 기본

---
+++ src/CHAPTER5/DoWhileBasic.java
@@ -0,0 +1,15 @@
+package CHAPTER5;
+
+public class DoWhileBasic {
+
+	public static void main(String[] args) {
+		int num1 = 0;
+		
+		do {
+			System.out.println("I love Java!" + num1);
+			num1++;
+		} while(num1<5);
+
+	}
+
+}
