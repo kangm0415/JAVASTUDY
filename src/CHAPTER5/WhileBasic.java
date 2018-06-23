7bfd35853af5f5c1b4560600f12f9a8c4f746976
강민 <@211.252.2.193>
2018-06-23 23:56:32 +0900

    z

--- src/CHAPTER5/WhileBasic.java
+++ src/CHAPTER5/WhileBasic.java
@@ -4,7 +4,7 @@ public class WhileBasic {
 
 	public static void main(String[] args) {
 		int num = 0;
-		while(num<5) {	//num<5 를 만족하면 반복 실행z
+		while(num<5) {	//num<5 를 만족하면 반복 실행
 			System.out.println("I like Java" + num);
 			num++;
 		}
