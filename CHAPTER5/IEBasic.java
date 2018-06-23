4416e922998d66d5cfe9eab21f7e0f65c6ff2f45
강민 <@211.252.2.193>
2018-06-23 18:59:06 +0900

    chapter5

---
+++ CHAPTER5/IEBasic.java
@@ -0,0 +1,23 @@
+
+public class IEBasic {
+
+	public static void main(String[] args) {
+		int n1=5;
+		int n2=9;
+				//if¹®
+		if(n1<n2) {
+			System.out.println("n1<n2 is true~"+'\n'+'\n');
+		}
+			//if~else ¹®	
+		if(n1==n2) {
+			System.out.println("n1==n2 is true~!");
+			
+		}
+		else {
+			System.out.println("n1==n2 is false, muther fucker");
+			
+		}
+
+	}
+
+}
