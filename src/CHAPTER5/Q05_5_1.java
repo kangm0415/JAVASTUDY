c9f77298fcbc4c0dfd168439ab76c902d012ccf5
강민 <@211.252.2.193>
2018-06-24 15:02:20 +0900

    for문 활용

---
+++ src/CHAPTER5/Q05_5_1.java
@@ -0,0 +1,15 @@
+package CHAPTER5;
+
+public class Q05_5_1 {
+
+	public static void main(String[] args) {
+		// 1부터 10까지의 곱의 결과를 출력하는 걸 for문을 사용하여 ㄱㄱ
+		int num=1;
+		for(int i = 1; i<11; i++) {
+			num*=i;
+		}
+		System.out.println(num);
+
+	}
+
+}
