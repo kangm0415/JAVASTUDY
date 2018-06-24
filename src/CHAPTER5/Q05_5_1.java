21a355cd94b8d46f460f23fe09af9d3a9f3cf999
강민 <@211.252.2.193>
2018-06-24 15:06:35 +0900

    for문 활용 두문제 한번에

--- src/CHAPTER5/Q05_5_1.java
+++ src/CHAPTER5/Q05_5_1.java
@@ -3,13 +3,18 @@ package CHAPTER5;
 public class Q05_5_1 {
 
 	public static void main(String[] args) {
-		// 1부터 10까지의 곱의 결과를 출력하는 걸 for문을 사용하여 ㄱㄱ
+		/* 1부터 10까지의 곱의 결과를 출력하는 걸 for문을 사용하여 ㄱㄱ
 		int num=1;
 		for(int i = 1; i<11; i++) {
 			num*=i;
 		}
-		System.out.println(num);
-
+		System.out.println(num); */
+		
+		//구구단 중 5단을 출력하는 프로그램을 for문을 사용해 작성
+		int num = 5;
+		for(int i = 1; i<10;i++) {
+			System.out.println(num +"*"+ i +"=" + num*i );
+		}
 	}
 
 }
