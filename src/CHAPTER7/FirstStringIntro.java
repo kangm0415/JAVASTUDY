44b833984fe5f44faf379815f301e4b69caa86d6
강민 <@211.252.3.107>
2018-07-07 19:40:45 +0900

    실제로 인스턴스를 생성하는 String

---
+++ src/CHAPTER7/FirstStringIntro.java
@@ -0,0 +1,16 @@
+package CHAPTER7;
+
+public class FirstStringIntro {
+	public static void main(String[] args) {
+		String str1 = "Happy";
+		String str2 = "birthday";
+		System.out.println(str1 + " " + str2);
+		
+		printString(str1);
+		printString(str2);
+	}
+
+		public static void printString(String str) {
+			System.out.println(str);
+		}
+}
